18e19dfcc0f51617c42111859c55a327a834c1ea
PedMatHhoven <pedro.matias@gymhueck.de>
2022-09-28 09:14:05 +0200

    StackGUI weiter

--- Code/_03_DynDs/StackMitGUI/Controller.java
+++ Code/_03_DynDs/StackMitGUI/Controller.java
@@ -1,4 +1,28 @@
 package _03_DynDs.StackMitGUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML private TextField tfIsEmpty;
+    @FXML private TextField tfPush;
+    @FXML private TextField tfTop;
+    private Stack <String> s;
+
+    public void btStack_click() {
+        s = new Stack <String> ();
+    }
+
+    public void btIsEmpty_click() {
+        if (s.isEmpty()) tfIsEmpty.setText("ja - leer!"); else tfIsEmpty.setText("nicht leer!");
+    }
+
+    public void btPush_click() {
+    }
+
+    public void btPop_click() {
+    }
+
+    public void btTop_click() {
+    }
 }
