1bae6f630b7e2e8cdfd5003762acbf115fadb238
PedMatHhoven <pedro.matias@gymhueck.de>
2022-11-10 11:05:25 +0100

    neue Listen

---
+++ Code/_03_DynDs/List/ListInt.java
@@ -0,0 +1,12 @@
+package _03_DynDs.List;
+
+public class ListInt <Integer> extends ListErw <Integer> {
+    public void toMax() {
+      //AT 2
+    }
+
+    public double average() {
+        //AT 1
+        return 0;
+    }
+}
