e759624c14110f6570c11ebeb8dad5f4c86ee887
PedMatHhoven <pedro.matias@gymhueck.de>
2022-11-30 08:57:49 +0100

    ListErw+ListInt-AB+Code

--- Code/_03_DynDs/List/mitGUIundInt/ListInt.java
+++ Code/_03_DynDs/List/mitGUIundInt/ListInt.java
@@ -33,17 +33,43 @@ public class ListInt extends ListErw <Integer> {
     public void searchLin(int x) {
         //current geht zu x (1. Auftreten) oder wird null
         toFirst();
-        while(hasAccess() && current.getContentObject() != x) next();
+        int zaehler=1; //zählt die Anzahl an Durchläufen
+        while(hasAccess() && getContent() != x) {
+            next();
+            zaehler ++;
+        }
+        System.out.println(zaehler);
     }
 
     public void searchBin(int x) {
         //Zahlen müssen in sortierter Form vorliegen!
         int l=1;
         int r=count();
-        //... m=(l+r)/2;
+        int zaehler=0; //zählt die Anzahl an Durchläufen
+        while (l<=r) {
+            int m = (l+r)/2; zaehler ++;
+            toFirst(); for (int i=1; i<m; i++) next(); //current geht zu "m"
+            if (getContent() < x) l=m+1;
+            else if (getContent() > x) r=m-1;
+            else l=r+2;
+        }
+        if (l==r+1) current = null;
+        System.out.println(zaehler);
     }
 
     public void sort() {
-
+        //BubbleSort
+        ListNode help = current; //zwischenspeichern
+        int zaehler=0; //zählt die Anzahl an Vergleichen
+        for (int i=1; i<count(); i++) {
+            toFirst();
+            for (int j=0; j<count()-i; j++) {
+                int nachbar = current.getNextNode().getContentObject();
+                if (getContent() > nachbar) tauscheRechts(); else next();
+                zaehler ++;
+            }
+        }
+        current = help; //zurücksetzen
+        System.out.println(zaehler);
     }
 }
