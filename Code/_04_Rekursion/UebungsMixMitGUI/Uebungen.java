cec7d13a993dee0e13870e89ca62a0b8b74bb48b
PedMatHhoven <pedro.matias@gymhueck.de>
2022-12-15 21:53:26 +0100

    AB zum Rekursions-Projekt/ kleine Layout-Änderungen

--- Code/_04_Rekursion/UebungsMixMitGUI/Uebungen.java
+++ Code/_04_Rekursion/UebungsMixMitGUI/Uebungen.java
@@ -15,7 +15,7 @@ public class Uebungen {
     if (a==1) return 1; else return a + SummeBisRek(a-1);
   }
 
-  public static int SummeBisIt(int a){
+  public static int SummeBisIt(int a) {
     int n=1;
     for (int i=2; i<=a; i++) n = n + i;
     return n;
@@ -26,17 +26,17 @@ public class Uebungen {
     else return a.charAt(a.length()-1) + SpiegelwortRek(a.substring(0,a.length()-1));
   }
 
-  public static String SpiegelwortIt(String a){
+  public static String SpiegelwortIt(String a) {
     String s = new String();
     for (int i=1; i<=a.length(); i++) s = s + a.charAt(a.length()-i);
     return s;
   }
 
-  public static int QuersummeRek(int a){
+  public static int QuersummeRek(int a) {
     if (a <= 9) return a; else return a%10 + QuersummeRek(a/10);
   }
 
-  public static int QuersummeIt(int a){
+  public static int QuersummeIt(int a) {
     int summe = 0;
     while (a > 0) {
       summe = summe + (a % 10);
@@ -45,24 +45,24 @@ public class Uebungen {
     return summe;
   }
 
-  public static int LineareSucheRek(int[] a, int b, int c){
+  public static int LineareSucheRek(int[] a, int b, int c) {
     if (b==a[c]) return c+1;
     else if(c==a.length-1) return 0;
     else return LineareSucheRek(a,b,c+1);
   }
   
-  public static int LineareSucheIt(int[] a, int b){
+  public static int LineareSucheIt(int[] a, int b) {
     for (int i=0; i<a.length; i++) if(a[i]==b) return i+1;
     return 0;
   }
   
-  public static int ggTRek(int a, int b){
+  public static int ggTRek(int a, int b) {
     if (a==0) return b;
     else if (a<b) return ggTRek(b-a, a);
     else return ggTRek(a-b, b);
   }
   
-  public static int ggTIt(int a, int b){
+  public static int ggTIt(int a, int b) {
     int ggT=1;
     int c=0;
     if (a>b) c=b; else c=a;
